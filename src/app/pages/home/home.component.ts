b1c0a4a617993e45c354527c9deba81949c989fe
Christine Bohnet <christine.bohnet@gmail.com>
2020-10-06 12:44:56 -0500

    added Item component to Array

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,17 +10,19 @@ import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { TaskService } from 'src/app/shared/task.service';
 import {HttpClient} from '@angular/common/http';
+import {Item} from '../../shared/item.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 
   //tasks: any;
-  todo: any;
-  done: any;
+  todo: Array<Item>;
+  done: Array<Item>;
 
   constructor(private taskService: TaskService, private http: HttpClient) {
    this.taskService.findAllTasks().subscribe(res => {
